92e7c7b35235b96b95959061e31dc4448903b9d7
Hamilton Lopez <hamilt727@gmail.com>
2025-05-31 10:21:58 -0500

    update

--- src/main/java/com/example/studentsupdate/StudentUpdateApplication.java
+++ src/main/java/com/example/studentsupdate/StudentUpdateApplication.java
@@ -1,1 +1,11 @@
- 
+package com.example.studentsupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+    public class StudentUpdateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentUpdateApplication.class, args);
+    }
+} 
